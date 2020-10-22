6d8cde2d0b4361416745f2e41f66e678828c1dbd
VivaHades <66169492+VivaHades@users.noreply.github.com>
2020-10-22 20:41:39 +0300

    Add files via upload

---
+++ parsen_window.R
@@ -0,0 +1,55 @@
+euclDistance = function(u,v){
+  sqrt(sum((u-v)^2))
+}
+
+gaussKer = function(r){
+  2*pi^((-1/2)*exp(-1/2*r^2))
+}
+
+distance = function(xl,z, metricFunction = euclDistance){
+  l = dim(xl)[1]
+  n = dim(xl)[2] - 1
+  dist = matrix(NA, l, 4)
+
+  for(i in 1:l){
+    dist[i, ] = c( metricFunction(xl[i, 1:n], z), xl[i, 1], xl[1, 2],xl[1, 3])
+  }
+  return (dist)
+}
+
+parsenWindow = function(z, xl, h, ker=gaussKer){
+  distances = distance(xl, z)
+  
+  l = dim(distances)[1]
+  n = dim(distances)[2] - 1
+  
+  classes = xl[1:l, n]
+  counts = table(classes)
+  counts[1:length(counts)] = 0
+  
+  for (i in 1:l) {
+    counts[distances[i,4]] <- counts[distances[i,4]] + ker(distances[i,1]/h)
+  }
+  
+  if(max(counts) != 0){
+    class = names(which.max(counts))
+    return (class)
+  }
+  
+  return (4)
+}
+
+colors = c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
+plot(iris[, 3:4], pch = 21, bg = colors[iris$Species],
+     col = colors[iris$Species], main="Parsen window")
+
+xl = iris[,3:5];
+
+
+for (x in seq(1, 7, 0.1)){
+  for (y in seq(0, 2.5, 0.1)){
+    z = c(x, y)
+    class = parsenWindow(z, xl, h=0.2)
+    points(z[1], z[2], pch = 22, col = colors[class])
+  }
+}
